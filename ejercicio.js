c25add17b4b1c08686a984e6f03361dfad2a549e
johan <johita12344779@gmail.com>
2025-03-12 20:20:28 -0500

    mi ejercicios

---
+++ ejercicio.js
@@ -0,0 +1,113 @@
+//1
+/*let j = [200, -100, 45, 78, 32]
+console.log(j[2],j[4])*/
+
+//2
+/*let f = ["ab", "cd", "ef", "gh"]
+console.log(f[1],f[3]);*/
+
+//3
+/*let aux = [10, true, "k200", 20.7]
+aux.forEach(aux =>{
+    console.log(aux);
+    
+})*/
+
+//4
+/*let k = [17, 4, 64, 79, 109, 112]
+k.forEach(n =>{
+    if(n % 2 !==0){
+        console.log(n);
+        
+    }
+})*/
+
+//5
+/*let h = [true, true, false,true, false]
+h [2] = true;
+h [3]= false;
+console.log(h);*/
+
+//6
+/*let w = ["wc", "jp", "zx", "qr"]
+w [1] = true;
+w [3] = 30;
+
+console.log(w);*/
+
+//7
+/*function re(arreglo){
+    arreglo.forEach(element => {
+        console.log(element);
+        
+    });
+}
+let resultado = [2, 5, 7, 9]
+re(resultado);*
+
+//8
+
+/*function rd(arreglo){
+    return arreglo.length;;
+}
+
+let m = [1,22,334,555,677]
+console.log(rd(m));*/
+
+//9
+
+/*function vLetra(Letra){
+    let arreglo = ["a", "b", "c", "d", "e", "f", "g"];
+    for(let i = 0; i < arreglo.lengt;i++){
+        if(arreglo[i] === Letra){
+            console.log(`la letra ${Letra} esta en el arreglo`);
+            return;
+        }
+    }
+    console.log(`la letra ${Letra} no esta en el arreglo`);
+    
+}
+
+vLetra("c")
+vLetra("z")*/
+
+
+
+
+
+//Dado el arreglo [3, 50, 70, 600, 40]:
+
+//1
+/*let arreglo =  [3, 50, 70, 600, 40]
+arreglo.forEach(i=>{
+    console.log(i);
+    
+})*/
+
+
+//2
+/*let arreglo = [3, 50, 70, 600, 40]
+arreglo.forEach(i =>{
+    console.log(i * 3);
+    
+})*/
+
+//3
+/*let arreglo = [3, 50, 70, 600, 40]
+let i = 0;
+while(i < arreglo.length){
+    console.log(arreglo[i]);
+    i++
+    
+}*/
+
+//4
+/*let arreglo = [3, 50, 70, 600, 40]
+for(let i = 0; i < arreglo.length; i++){
+    if(i%2==0){
+        console.log(` ${arreglo[i]}`);
+        
+    }
+}*/
+
+
